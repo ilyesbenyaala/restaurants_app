3046918dbbb3e7d039a02848c64a8d878f0f90e4
ilyes <ilyesben2009@hotmail.com>
2023-06-11 18:29:33 +0100

    Updates

---
+++ precache-manifest.da8a42b2e908b248ff54429a8b2a600e.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = [
+  {
+    "revision": "2503320c82fa3309983f",
+    "url": "/restaurants_app/static/css/main.830dd8c1.chunk.css"
+  },
+  {
+    "revision": "2503320c82fa3309983f",
+    "url": "/restaurants_app/static/js/main.e6ad2fed.chunk.js"
+  },
+  {
+    "revision": "c3d56dab9eb9ecc7b927",
+    "url": "/restaurants_app/static/js/runtime~main.09219588.js"
+  },
+  {
+    "revision": "bf697894fc66e544a9d9",
+    "url": "/restaurants_app/static/js/2.a3d58c29.chunk.js"
+  },
+  {
+    "revision": "9b815df02ea6dffc27ae4df2fc2faa0e",
+    "url": "/restaurants_app/static/media/lisbon.9b815df0.jpg"
+  },
+  {
+    "revision": "591e7b67bbed8aed87a02f77d163ce96",
+    "url": "/restaurants_app/static/media/illustration-portugal-flag_53876-18170.jpg.591e7b67.avif"
+  },
+  {
+    "revision": "9af5f4d6d8bc86e87d9f01c6f9096cbf",
+    "url": "/restaurants_app/static/media/lisbon_cuisine.9af5f4d6.jpg"
+  },
+  {
+    "revision": "b0464de7dc99ca76258c175e277cd8da",
+    "url": "/restaurants_app/static/media/food.b0464de7.jpg"
+  },
+  {
+    "revision": "a65cce549222e51421c75e081770a6cd",
+    "url": "/restaurants_app/static/media/100.a65cce54.jpg"
+  },
+  {
+    "revision": "58cc6e89176867ff84ffc45250c4f859",
+    "url": "/restaurants_app/static/media/Bairro-do-Avillez-Lisbon-Portugal-02.58cc6e89.jpg"
+  },
+  {
+    "revision": "dda2195b7dc20446122b655c86992eed",
+    "url": "/restaurants_app/static/media/Loco-Lisboa.-Chef-Alexandre-Silva-1-1.dda2195b.jpg"
+  },
+  {
+    "revision": "a1f9808720315954fbfbf7a1fbb59305",
+    "url": "/restaurants_app/static/media/Sunset-at-Ponto-Final-in-Lisbon.a1f98087.jpg"
+  },
+  {
+    "revision": "eb86e9c4e68f35bcdd56a2046458b6ea",
+    "url": "/restaurants_app/static/media/alma.eb86e9c4.jpg"
+  },
+  {
+    "revision": "1f2e49d001dcbde9a40cf01042a613f8",
+    "url": "/restaurants_app/static/media/avillez.1f2e49d0.jpeg"
+  },
+  {
+    "revision": "b471084c5970279cc9a887a27e8134c0",
+    "url": "/restaurants_app/static/media/avillez_ca.b471084c.jpg"
+  },
+  {
+    "revision": "d2fe1b09c66e2df5e11da28539f2ef24",
+    "url": "/restaurants_app/static/media/barrio.d2fe1b09.webp"
+  },
+  {
+    "revision": "7d2ec0c5282bda28dd4d95ce022f6bdb",
+    "url": "/restaurants_app/static/media/belcanto.7d2ec0c5.jpg"
+  },
+  {
+    "revision": "bc1fa53bf8f34d36958ec0dae20b4fe7",
+    "url": "/restaurants_app/static/media/cevicheria.bc1fa53b.jpg"
+  },
+  {
+    "revision": "17e436e20d3088f4f519ea9582e7a6c7",
+    "url": "/restaurants_app/static/media/eleven.17e436e2.jpg"
+  },
+  {
+    "revision": "c46ddd5a631bd181f3490500ec79790c",
+    "url": "/restaurants_app/static/media/feitoria.c46ddd5a.jpg"
+  },
+  {
+    "revision": "573b396115b4299103adfab5b3668305",
+    "url": "/restaurants_app/static/media/folores.573b3961.jpg"
+  },
+  {
+    "revision": "10748a53f6f1cfc292f44f6c08b5721e",
+    "url": "/restaurants_app/static/media/min.10748a53.jpeg"
+  },
+  {
+    "revision": "6fb751eced257f6c3c5b99ca2a450a81",
+    "url": "/restaurants_app/static/media/mini.6fb751ec.jpg"
+  },
+  {
+    "revision": "f63018259d9768becad427ba39f3793a",
+    "url": "/restaurants_app/static/media/ramiro.f6301825.jpg"
+  },
+  {
+    "revision": "78d237e55fedf7034268226256e4ab56",
+    "url": "/restaurants_app/static/media/solar.78d237e5.jpg"
+  },
+  {
+    "revision": "8270f8e74ef5a70d2451fb7d2c8df34f",
+    "url": "/restaurants_app/static/media/taberna.8270f8e7.jpeg"
+  },
+  {
+    "revision": "fc11adaa7e1939c7c121cf37c3115a83",
+    "url": "/restaurants_app/static/media/taberna.fc11adaa.webp"
+  },
+  {
+    "revision": "c363c2c8e71fea4b129a8e6b8141b644",
+    "url": "/restaurants_app/static/media/brazil.c363c2c8.jpeg"
+  },
+  {
+    "revision": "8fcb0ca9bf33d66abd5687fa1cd4935e",
+    "url": "/restaurants_app/static/media/indian.8fcb0ca9.jpeg"
+  },
+  {
+    "revision": "f6728802f503544357210788c8a49693",
+    "url": "/restaurants_app/static/media/italian-cuisine-47-1440x600.f6728802.jpg"
+  },
+  {
+    "revision": "56eda3062c32a9cf48c249e0ae84a9d9",
+    "url": "/restaurants_app/static/media/japan.56eda306.jpg"
+  },
+  {
+    "revision": "f4abdb35622d73628b5bd9ee3cc58104",
+    "url": "/restaurants_app/static/media/med.f4abdb35.jpg"
+  },
+  {
+    "revision": "00a861cb497ce282198b19981a6979d0",
+    "url": "/restaurants_app/static/media/mex.00a861cb.jpg"
+  },
+  {
+    "revision": "8ca37839ead4809e9705adf6a2b13b22",
+    "url": "/restaurants_app/static/media/pratoscozinhaportuguesa630x420.8ca37839.jpg"
+  },
+  {
+    "revision": "bacc6afba5915cd9166cd91b18264ef0",
+    "url": "/restaurants_app/index.html"
+  }
+];
